a9062f184b259e60897bf21f255df04b8006267b
sarmadi <khaled.ibrahim@finaira.ai>
2026-08-02 16:26:35 +0300

    removing standalone dcr and vlm containers

--- containers/peyk-vlm/backends/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import VLMBackend, VLMResult
-from .registry import MODEL_REGISTRY, PROVIDER_CLASSES, get_backend
-
-__all__ = ["VLMBackend", "VLMResult", "MODEL_REGISTRY", "PROVIDER_CLASSES", "get_backend"]
